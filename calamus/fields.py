d7cab65c1c7df0fc97a29946e339c00c14305e2b
Panaetius <ralf.grubenmann@sdsc.ethz.ch>
2020-07-01 10:28:53 +0200

    Reuse original data on proxy serialisation if proxy isn't initialised

--- calamus/fields.py
+++ calamus/fields.py
@@ -19,7 +19,6 @@
 
 from functools import total_ordering
 
-import lazy_object_proxy
 import marshmallow.fields as fields
 from marshmallow.base import SchemaABC
 from marshmallow import class_registry, utils
@@ -31,7 +30,7 @@ from uuid import uuid4
 import typing
 import types
 
-from calamus.utils import normalize_type, normalize_value
+from calamus.utils import normalize_type, normalize_value, Proxy
 
 logger = logging.getLogger("calamus")
 
@@ -302,7 +301,16 @@ class Nested(_JsonLDField, fields.Nested):
 
     def _serialize_single_obj(self, obj, **kwargs):
         """Deserializes a single (possibly flattened) object."""
-        if isinstance(obj, lazy_object_proxy.Proxy):
+        if isinstance(obj, Proxy):
+            proxy_schema = obj.__proxy_schema__
+            matching_schema = next((s for s in self.schema["to"].values() if isinstance(proxy_schema, type(s))), None)
+            if (
+                not obj.__proxy_initialized__
+                and matching_schema
+                and proxy_schema.flattened == matching_schema.flattened
+            ):
+                # if proxy was not accessed and we use the same schema, return original data
+                return obj.__proxy_original_data__
             # resolve Proxy object
             obj = obj.__wrapped__
 
@@ -316,9 +324,22 @@ class Nested(_JsonLDField, fields.Nested):
         """Deserialize a nested field with one or many values."""
         if nested_obj is None:
             return None
-        if isinstance(nested_obj, lazy_object_proxy.Proxy):
+
+        if isinstance(nested_obj, Proxy):
+            proxy_schema = nested_obj.__proxy_schema__
+            matching_schema = next((s for s in self.schema["to"].values() if isinstance(proxy_schema, type(s))), None)
+
+            if (
+                not nested_obj.__proxy_initialized__
+                and matching_schema
+                and proxy_schema.flattened == matching_schema.flattened
+            ):
+                # if proxy was not accessed and we use the same schema, return original data
+                return nested_obj.__proxy_original_data__
+
             # resolve Proxy object
             nested_obj = nested_obj.__wrapped__
+
         many = self.many or many
         if many:
             result = []
@@ -344,7 +365,7 @@ class Nested(_JsonLDField, fields.Nested):
             ValueError("Type {} not found in {}.{}".format(value["@type"], type(self.parent), self.data_key))
 
         if schema.lazy:
-            return lazy_object_proxy.Proxy(lambda: schema.load(value, unknown=self.unknown, partial=partial))
+            return Proxy(lambda: schema.load(value, unknown=self.unknown, partial=partial), schema, value)
         return schema.load(value, unknown=self.unknown, partial=partial)
 
     def _load(self, value, data, partial=None, many=False):
